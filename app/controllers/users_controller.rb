9858815c5fc1f97481616f90c5ba15a39e427f01
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-11-21 20:43:39 +0100

    Remove actions from UsersController (were already handled by Devise): keep only Show
    Signed-off-by: Irvin TORTERAT <irvin.torterat@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
-
-  def show; end
-
-  def edit; end
-
-  def update
-    if @user.update(user_params)
-      redirect_to @user, notice: "User was successfully updated."
-    else
-      render 'edit', status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @user.destroy
-    redirect_to root_path, notice: "User was successfully destroyed."
-  end
-
-  private
-
-  def set_user
+  def show
     @user = User.find(params[:id])
   end
 end
