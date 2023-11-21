a96f8df309f0aa7b3e967d54e6c1f8aacb475f69
Rym Badji <rym.badji@gmail.com>
2023-11-21 14:18:46 +0100

    alibi seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,27 @@
 #   Character.create(name: "Luke", movie: movies.first)
 puts 'Creating 20 fake users...'
 20.times do
-  users = User.new(
+  user = User.new(
     first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
   )
-  User.save!
+  user.save!
+end
+
+users = User.all
+
+40.times do
+  Alibi.create(
+    title: Faker::Games::Pokemon.move,
+    content: Faker::TvShows::Simpsons.quote,
+    status: %w[family date journey speak cinema].sample,
+    category: %w[valide archived].sample,
+    date: Faker::Date.between(from: '2023-11-24', to: '2026-09-25'),
+    image: Faker::Avatar.image,
+    price: rand(1..10000),
+    user_id: users[rand(1..20)]
+  )
 end
 puts 'User create!'
