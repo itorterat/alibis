9161be549623017b27da5a204a24e18418d65a52
lilian annette <lilianannettepro@gmail.com>
2023-11-22 12:22:10 +0100

    Creating seed for pitch and test

--- app/models/alibi.rb
+++ app/models/alibi.rb
@@ -6,12 +6,12 @@ class Alibi < ApplicationRecord
 
   enum status: { available: 0, archived: 1 }
 
-  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
-  validates :content, presence: true, length: { minimum: 10 }
-  validates :status, inclusion: { in: statuses.keys }
-  # validates :image, presence: true
-  validates :category, inclusion: { in: CATEGORIES }
-  validates :date, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
-  validates :user, presence: true
+  # validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  # validates :content, presence: true, length: { minimum: 10 }
+  # validates :status, inclusion: { in: statuses.keys }
+  # # validates :image, presence: true
+  # validates :category, inclusion: { in: CATEGORIES }
+  # validates :date, presence: true
+  # validates :price, numericality: { greater_than_or_equal_to: 0 }
+  # validates :user, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,66 @@
-puts 'Creating 20 fake users...'
+puts 'Creating 2 user for pitch and test...'
+
+User.create(
+  first_name: "John",
+  last_name: "Smith",
+  email: "john@smith.com",
+  password: "password"
+)
+User.create(
+  first_name: "Jane",
+  last_name: "Doe",
+  email: "jane@doe.com",
+  password: "password"
+)
+
+puts "User created !"
+
+puts "Creating 4 alibis for the pitch and test..."
+
+Alibi.create(
+  title: "Recherche une femme",
+  content: "J'ai fais croire a mes parents que j'avais une petite ami et maintenant il veulent la voir j'ai donc besoin d'une personne qui pourra m'aider",
+  status: :available,
+  category: "family",
+  date: Time.now + 5.day,
+  image: "",
+  price: 1000,
+  user_id: 1
+)
+Alibi.create(
+  title: "Besoin d'une Homme ",
+  content: "J'ai une rendez-vous professionnel mon ex sera présent et je pour faire croire a mon ex que j'ai tourné la page",
+  status: :available,
+  category: "speak",
+  date: Time.now + 2.day,
+  image: "",
+  price: 5000,
+  user_id: 2
+)
+Alibi.create(
+  title: "Sortie cinéma",
+  content: "J'ai besoin de companie pour ",
+  status: :archived,
+  category: "date",
+  date: Time.now + 7.day,
+  image: "",
+  price: 70,
+  user_id: 1
+)
+Alibi.create(
+  title: "J'ai besoin de faire des photos",
+  content: "Je suis créatrice de contenu et je veux faire le couple Goal donc je cherche quelqu'un pour poser avec moi",
+  status: :archived,
+  category: "date",
+  date: Time.now + 9.day,
+  image: "",
+  price: 1870,
+  user_id: 2
+)
+puts "Alibis created !"
+
+puts "Creating 20 users by Faker"
+
 20.times do
   User.create!(
     first_name: Faker::Name.first_name,
@@ -7,6 +69,7 @@ puts 'Creating 20 fake users...'
     password: Faker::Internet.password
   )
 end
+
 puts 'Users created!'
 
 users = User.all
@@ -16,7 +79,7 @@ puts 'Creating 40 fake alibis...'
   Alibi.create!(
     title: Faker::Games::Pokemon.move,
     content: Faker::TvShows::Simpsons.quote,
-    status: %i[available archived].sample,
+    status: [0, 1].sample,
     category: %w[family date journey speak cinema].sample,
     date: Faker::Date.between(from: '2023-11-24', to: '2026-09-25'),
     image: Faker::Avatar.image,
