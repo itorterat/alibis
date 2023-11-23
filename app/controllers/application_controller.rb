919f76c11aeda5e1b9653132cf97d5c3e20f98fc
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-11-23 22:19:46 +0100

    Add avatar system to user
    Signed-off-by: Irvin TORTERAT <irvin.torterat@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name photo])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name photo])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :bookings
   has_many :alibis
+
+  has_one_attached :photo
 end
