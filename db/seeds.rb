90f3015917a9c644102e559bc0fa1993c1981141
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-11-23 15:26:51 +0100

    Add goal to alibi, need front
    Signed-off-by: Irvin TORTERAT <irvin.torterat@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 puts 'Creating 2 users'
-User.create(
+User.create!(
   first_name: "John",
   last_name: "Smith",
   email: "john@smith.com",
   password: "password"
 )
-User.create(
+User.create!(
   first_name: "Jane",
   last_name: "Doe",
   email: "jane@doe.com",
@@ -14,10 +14,11 @@ User.create(
 puts "Users created !"
 
 puts "Creating 4 alibis"
-Alibi.create(
+Alibi.create!(
   title: "Recherche une femme",
   content: "J'ai fais croire a mes parents que j'avais une petite ami et maintenant il veulent la voir j'ai donc besoin d'une personne qui pourra m'aider",
   status: :available,
+  goal: :ask,
   category: "family",
   date: Time.now + 5.day,
   image: "",
@@ -25,30 +26,33 @@ Alibi.create(
   user: User.find_by(email: 'john@smith.com')
 )
 
-Alibi.create(
+Alibi.create!(
   title: "Besoin d'une Homme ",
   content: "J'ai une rendez-vous professionnel mon ex sera présent et je pour faire croire a mon ex que j'ai tourné la page",
   status: :available,
+  goal: :ask,
   category: "speak",
   date: Time.now + 2.day,
   image: "",
   price: 5000,
   user: User.find_by(email: 'jane@doe.com')
 )
-Alibi.create(
+Alibi.create!(
   title: "Sortie cinéma",
   content: "J'ai besoin de companie pour ",
   status: :archived,
+  goal: :ask,
   category: "date",
   date: Time.now + 7.day,
   image: "",
   price: 70,
   user: User.find_by(email: 'john@smith.com')
 )
-Alibi.create(
+Alibi.create!(
   title: "J'ai besoin de faire des photos",
   content: "Je suis créatrice de contenu et je veux faire le couple Goal donc je cherche quelqu'un pour poser avec moi",
   status: :archived,
+  goal: :ask,
   category: "date",
   date: Time.now + 9.day,
   image: "",
@@ -74,7 +78,8 @@ users = User.all
   Alibi.create!(
     title: Faker::Games::Pokemon.move,
     content: Faker::TvShows::Simpsons.quote + '.....',
-    status: [0, 1].sample,
+    status: %i[archived available].sample,
+    goal: %i[ask offer].sample,
     category: %w[family date journey speak cinema].sample,
     date: Faker::Date.between(from: '2023-11-24', to: '2026-09-25'),
     image: Faker::Avatar.image,
@@ -86,7 +91,6 @@ puts 'Alibis from Faker created!'
 
 puts 'Creating 1 booking'
 Booking.create!(
-  status: :pending,
   user: User.find_by(email: 'jane@doe.com'),
   alibi: Alibi.find_by(title: 'Recherche une femme')
 )
